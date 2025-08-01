c24fb496a73417eb6370694a45fce1afd48bed2f
AshtonHill <30095596@tafe.wa.edu.au>
2025-08-01 09:29:57 +0800

    InitialCommit

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val available = listOf("Coffee", "Milk", "")
+    val order = listOf("")
+}
+fun calculateCaffeineLevel(hoursWorked: Int, bugsFound: Int, energy: String): String {
+    return readLine()!!
+}
+fun canFulfillOrder(orderItems: List<String>, availableIngredients: List<String>):Boolean{
+    if(){
+        return true
+    } else {
+        return false
+    }
+}
+
+fun createDrinkName(): String {
+
+}
